957113802d44da86391128e4ddf92a67ccbadced
André_Lucas <134009910+22346-Andre@users.noreply.github.com>
2026-07-09 21:59:40 -0300

    Update RelatorioController.java

--- backend/backend/src/main/java/com/smartstock/backend/controller/RelatorioController.java
+++ backend/backend/src/main/java/com/smartstock/backend/controller/RelatorioController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/relatorios") // 🟢 CORRIGIDO! Removido o "/api"
+@RequestMapping("/relatorios") 
 public class RelatorioController {
 
     private final RelatorioPdfService relatorioPdfService;
